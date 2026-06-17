66c259071d2a9eb5dc8cfa2edf21d116a4e114bc
Pat <patofmardi@gmail.com>
2026-06-17 15:06:53 +1000

    Started assignment 2

---
+++ PatrickTriggellAssign2.py
@@ -0,0 +1,27 @@
+#Author: <Patrick Triggell>
+#Date: <17/6/26> – <upload date>
+#Task: INFT1004 Assignment 2 – Library Management System – Part 2
+
+import pandas as pd
+import matplotlib
+
+def load_inventory_from_csv():
+    pass
+
+def display_inventory():
+    pass
+
+def add_new_genre():
+    pass
+
+def checkout_or_return():
+    pass
+
+def inventory_analysis_and_visualisation():
+    pass
+
+def save_inventory_to_csv():
+    pass
+
+def menu():
+    pass
